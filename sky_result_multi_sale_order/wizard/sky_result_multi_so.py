f25858d6e850e8c2f3bcd1df1a222b01398d9b3d
Vo Van Tin <vovantinit>
2017-01-04 09:18:33 +0700

    Sua ketqua thanh ngay tinh doanh so

--- sky_result_multi_sale_order/wizard/sky_result_multi_so.py
+++ sky_result_multi_sale_order/wizard/sky_result_multi_so.py
@@ -6,7 +6,8 @@ from openerp.exceptions import ValidationError
 class sky_result_multi_so(models.TransientModel):
     _name = 'sky_result_multi_so'
 
-    ketqua = fields.Selection([('pass','Pass'), ('fall','Fall'), ('canceled','Cancelled')], string="Result")
+    # ketqua = fields.Selection([('pass','Pass'), ('fall','Fall'), ('canceled','Cancelled')], string="Result")
+    date_result = fields.Date('Date result', required=True)
 
     @api.multi
     def submit(self):
@@ -14,5 +15,9 @@ class sky_result_multi_so(models.TransientModel):
         if not self._context.get('active_model', False) == 'sale.order':
             return
 
-        so_ids = self._context.get('active_ids')
-        self.env['sale.order'].browse(so_ids).write({'ketqua': self.ketqua})
+        so_ids = self.env['sale.order'].browse(self._context.get('active_ids'))
+        has_result = so_ids.filtered(lambda so: so.x_ngaytinhdoanhso or so.ketqua)
+        if has_result:
+            so_names = ','.join(has_result.mapped('name'))
+            raise ValidationError(_('These orders have results or days of sales ') + so_names)
+        so_ids.write({'x_ngaytinhdoanhso': self.date_result})
