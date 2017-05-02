6c6d445c0c9bb90d1642986b52334cf591726428
Vo Van Tin <vovantinit>
2017-05-02 20:36:52 +0700

    Phan quyen

--- sky_forwarder/controllers/main.py
+++ sky_forwarder/controllers/main.py
@@ -13,14 +13,13 @@ class GiaoNhanModule(http.Controller):
             return http.request.redirect("/web/login?redirect=giao_nhan")
 
         Models = http.request.env['sky.forwarder']
-        # group_forwarder_nhan_vien
-        # group_forwarder_user
         is_manager = http.request.env['res.users'].has_group('sky_forwarder.group_forwarder_user')
-
         user_ids = http.request.env.ref('sky_forwarder.group_forwarder_nhan_vien').users
 
-        domain = ['|', ('forwarder_id', 'in', (False, http.request.env.user.id)), (is_manager, '=', True)]
+        # domain = ['|', ('forwarder_id', 'in', (False, http.request.env.user.id)), (is_manager, '=', True)]
         # domain = [('forwarder_id', 'in', (False, http.request.env.user.id))]
+
+        domain = []
 
         thong_ke = []
         for nv in user_ids:
@@ -30,44 +29,49 @@ class GiaoNhanModule(http.Controller):
                 'sl_giao': Models.search_count([('s_date','=',str(date.today())),('forwarder_id','=',nv.id),('delivered','=',True)]),
             })
 
-        offset = (int(post.get('page', 1)) - 1) * 2
+        offset = (int(post.get('page', 1)) - 1) * 20
 
         return http.request.render('sky_forwarder.index', {
             'user': http.request.env.user.id,
             'user_ids': user_ids,
-            'datas': Models.search(domain, order=order, limit=2, offset=offset),
+            'datas': Models.search(domain, order=order, limit=20, offset=offset),
             'is_manager': is_manager,
             'thong_ke': thong_ke,
             'page': post.get('page', 1),
+            'error': post.get('error', False),
+            'success': post.get('success', False),
         })
 
 
     @http.route('/gan_giao_nhan', auth='user', website=True)
     def gan_giao_nhan(self, **post):
+        try:
+            if not http.request.env.user:
+                return http.request.redirect("/web/login")
 
-        if not http.request.env.user:
-            return http.request.redirect("/web/login")
+            Models = http.request.env['sky.forwarder']
 
-        Models = http.request.env['sky.forwarder']
+            if post.get('record_id', False):
+                res = Models.browse(int(post.get('record_id', False)))
+                record_type = post.get('record_type', False)
+                delivered   = post.get('delivered', False)
+               
+                if record_type == 'set':
+                    res.write({'forwarder_id': int(post.get('to_id', False))} )
 
-        if post.get('record_id', False):
-            res = Models.browse(int(post.get('record_id', False)))
-            record_type = post.get('record_type', False)
-            delivered   = post.get('delivered', False)
-           
-            if record_type == 'set':
-                res.write({'forwarder_id': int(post.get('to_id', False))} )
+                if record_type == 'del':
+                    res.write({'forwarder_id': False})            
 
-            if record_type == 'del':
-                res.write({'forwarder_id': False})            
+                if delivered == 't':
+                    res.write({'delivered': True})
 
-            if delivered == 't':
-                res.write({'delivered': True})
+                if delivered == 'f':
+                    res.write({'delivered': False})
 
-            if delivered == 'f':
-                res.write({'delivered': False})
+            return http.request.redirect("/giao_nhan?success=1")
 
-        return http.request.redirect("/giao_nhan")
+        except Exception as e:
+            return http.request.redirect("/giao_nhan?error=1")
 
     @http.route('/ghichu', auth='user', website=True)
     def ghichu(self, **post):
